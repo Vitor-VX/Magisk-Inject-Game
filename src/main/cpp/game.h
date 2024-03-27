a8305c3a15c4aee2152f9b75d8b35bd570eccb4a
if_vitor <127361991+Vitor-VX@users.noreply.github.com>
2024-03-27 18:06:43 -0400

    Update game.h

--- src/main/cpp/game.h
+++ src/main/cpp/game.h
@@ -1,6 +1,6 @@
-#ifndef ZYGISK_IL2CPPDUMPER_GAME_H
-#define ZYGISK_IL2CPPDUMPER_GAME_H
+#ifndef ZYGISK_GAME_H
+#define ZYGISK_GAME_H
 
 #define GamePackageName "com.netmarble.skiagb"
 
-#endif //ZYGISK_IL2CPPDUMPER_GAME_H
+#endif
