e81e93a0bb0562ad508adb82c7ea8813994d4b44
if_vitor <127361991+Vitor-VX@users.noreply.github.com>
2024-03-27 20:10:18 -0400

    Delete src/main directory

--- src/main/cpp/game.h
+++
@@ -1,6 +0,0 @@
-#ifndef ZYGISK_GAME_H
-#define ZYGISK_GAME_H
-
-#define GamePackageName "com.netmarble.skiagb"
-
-#endif
--- src/main/cpp/hook.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ZYGISK_HACK_H
-#define ZYGISK_HACK_H
-
-#include <stddef.h>
-
-void hack_prepare(const char *game_data_dir, void *data, size_t length);
-
-#endif //
